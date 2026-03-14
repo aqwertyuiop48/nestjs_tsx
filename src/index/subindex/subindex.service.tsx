b18dd1ce2273aadb2e5d03a631f06130ba87c6e4
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2026-03-14 17:53:46 +0530

    Update internal URLs

--- src/index/subindex/subindex.service.tsx
+++ src/index/subindex/subindex.service.tsx
@@ -2220,7 +2220,7 @@ export class IndexService {
       };
       sprite = new Image();
       sprite.src =
-        "https://image.ibb.co/dr1HZy/Pf_RWr3_X_Imgur.png" +
+        "https://i.imgur.com/PfRWr3X.png" +
         "?" +
         new Date().getTime();
       sprite.setAttribute("crossOrigin", " ");
@@ -2232,7 +2232,7 @@ export class IndexService {
       };
     
       finishSprite = new Image();
-      finishSprite.src = "https://image.ibb.co/b9wqnJ/i_Q7m_U25_Imgur.png"+
+      finishSprite.src = "https://i.imgur.com/iQ7mU25.png"+
       "?" +
       new Date().getTime();
       finishSprite.setAttribute("crossOrigin", " ");
