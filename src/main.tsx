3197ad7f3247aedfd9c9a55e32caba3a83171a29
aqwertyuiop48 <aqwertyuiop48@hotmail.com>
2026-05-08 05:28:32 +0000

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
-import * as express from 'express';
 
-const server = express();
-
-export const createNestServer = async (expressInstance: express.Express) => {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-  );
-  app.enableCors();
-  await app.init();
-  return app;
-};
-
-// Initialize the server
-createNestServer(server)
-  .then(() => console.log('Nest Ready'))
-  .catch((err) => console.error('Nest broken', err));
-
-// Export for Vercel
-export default server;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3002);
+}
+bootstrap();
